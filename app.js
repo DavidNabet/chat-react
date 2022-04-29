7d54af21d6d27d1caa7052398cfd020091d74863
DavidNabet <david97.nabet@gmail.com>
2022-04-29 19:49:30 +0200

    updates and heading sass

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const createError = require('http-errors');
-const path = require('path');
+require("dotenv").config();
+const express = require("express");
+const createError = require("http-errors");
+const path = require("path");
+const morgan = require("morgan");
 
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-require('dotenv').config();
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -16,31 +14,30 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "mustache");
 
 //partials
-app.engine("mustache", require('hogan-middleware').__express)
+app.engine("mustache", require("hogan-middleware").__express);
 
-app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,100 @@
-const gulp = require("gulp")
-const gulpSass = require("gulp-sass")
-const gulpClean = require("gulp-clean-css")
-const terser = require("gulp-terser")
-const autoprefixer = require('gulp-autoprefixer')
-const concat = require("gulp-concat")
-const image = require('gulp-image')
-const replaceName = require('gulp-replace-name')
+const gulp = require("gulp");
+const gulpSass = require("gulp-sass");
+const gulpClean = require("gulp-clean-css");
+const terser = require("gulp-terser");
+const autoprefixer = require("gulp-autoprefixer");
+const concat = require("gulp-concat");
+const image = require("gulp-image");
+const replaceName = require("gulp-replace-name");
 
 // compile sass file to CSS
-gulp.task('sass', function() {
-    return gulp.src('./public/scss/*.scss')
-        .pipe(gulpSass())
-        // .pipe(gulpAutoprefixer({
-        //     browsers: 'last 6 versions'
-        // }))
-        .pipe(gulpClean({
-            compatibility: 'ie11'
-        }))
-        .pipe(gulp.dest('./public/css/'))
+gulp.task("sass", function () {
+  return (
+    gulp
+      .src("./public/scss/*.scss")
+      .pipe(gulpSass())
+      // .pipe(gulpAutoprefixer({
+      //     browsers: 'last 6 versions'
+      // }))
+      .pipe(
+        gulpClean({
+          compatibility: "ie11",
+        })
+      )
+      .pipe(gulp.dest("./public/css/"))
+  );
 });
 
 // concat all css files in one file
-gulp.task('css', function() {  // Tache permettant de minifier toutes les feuilles de style en un seul fichier min "style.min.css"
-    return gulp.src(
-        [
-            './public/css/fontawesome-all.min.css',
-            './public/css/fa-brands.min.css',
-            './public/css/fa-light.min.css',
-            './public/css/fa-regular.min.css',
-            './public/css/fa-solid.min.css',
-            './public/css/font-face.css',
-            './public/css/base.css'
-        ]) 
-        // .pipe(gulpAutoprefixer({ // Tache ajoutant les autopréfixer
-        //     browsers: 'last 6 versions'
-        // }))
-        .pipe(gulpClean({
-            compatibility: 'ie11'
-        }))
-        .pipe(concat('style.min.css')) // On concaténe tous nos fichiers css en un fichier css "style.min.js"
-        .pipe(gulp.dest('./public/dist/css/'))
+gulp.task("css", function () {
+  // Tache permettant de minifier toutes les feuilles de style en un seul fichier min "style.min.css"
+  return (
+    gulp
+      .src([
+        "./public/css/fontawesome-all.min.css",
+        "./public/css/fa-brands.min.css",
+        "./public/css/fa-light.min.css",
+        "./public/css/fa-regular.min.css",
+        "./public/css/fa-solid.min.css",
+        "./public/css/font-face.css",
+        "./public/css/base.css",
+      ])
+      // .pipe(gulpAutoprefixer({ // Tache ajoutant les autopréfixer
+      //     browsers: 'last 6 versions'
+      // }))
+      .pipe(
+        gulpClean({
+          compatibility: "ie11",
+        })
+      )
+      .pipe(concat("style.min.css")) // On concaténe tous nos fichiers css en un fichier css "style.min.js"
+      .pipe(gulp.dest("./public/dist/css/"))
+  );
 });
 
 // minify js files
-gulp.task('js', function(){
-    return gulp.src('./public/js/*.js')
+gulp.task("js", function () {
+  return gulp
+    .src("./public/js/*.js")
 
-        .pipe(concat('main.min.js'))
-        .pipe(terser())
-        .pipe(gulp.dest('./public/dist/js/'))
-        // .pipe(uglify())
-        // .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+    .pipe(concat("main.min.js"))
+    .pipe(terser())
+    .pipe(gulp.dest("./public/dist/js/"));
+  // .pipe(uglify())
+  // .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
 });
 
 // Fonts
-gulp.task('fonts', function() {
-    return gulp.src('./public/fonts/*')
-        .pipe(gulp.dest('./public/dist/fonts/'))
+gulp.task("fonts", function () {
+  return gulp.src("./public/fonts/*").pipe(gulp.dest("./public/dist/fonts/"));
 });
 
 // Minify Images
-gulp.task('image', function() {
-    return gulp.src('./public/images/**/*')
-        .pipe(image())
-        .pipe(gulp.dest('./public/dist/images/'));
+gulp.task("image", function () {
+  return gulp
+    .src("./public/images/**/*")
+    .pipe(image())
+    .pipe(gulp.dest("./public/dist/images/"));
 });
 
 // Replace elements in filename
 // space -> _
-gulp.task('replaceName', function() {
-    return gulp.src('./public/dist/images/**/*')
-        .pipe(replaceName(/ /g, '_'))
-        .pipe(gulp.dest('./public/dist/images/'));
+gulp.task("replaceName", function () {
+  return gulp
+    .src("./public/dist/images/**/*")
+    .pipe(replaceName(/ /g, "_"))
+    .pipe(gulp.dest("./public/dist/images/"));
 });
 
-
-gulp.task('watch', function() { // Détecter tous les changements en tapant la commande "gulp watch" dans le terminal
-    gulp.watch('./public/scss/*.scss', gulp.series('sass'))
-    gulp.watch('./public/css/*', gulp.series('css'))
-    gulp.watch('./public/js/*', gulp.series('js'))
-    gulp.watch('./public/fonts/*', gulp.series('fonts'))
+gulp.task("watch", function () {
+  // Détecter tous les changements en tapant la commande "gulp watch" dans le terminal
+  gulp.watch("./public/scss/*.scss", gulp.series("sass"));
+  gulp.watch("./public/css/*", gulp.series("css"));
+  gulp.watch("./public/js/*", gulp.series("js"));
+  gulp.watch("./public/fonts/*", gulp.series("fonts"));
 });
 
-gulp.task('default', gulp.series('sass', 'css', 'js', 'fonts', 'image', 'replaceName'));
+gulp.task(
+  "default",
+  gulp.series("sass", "css", "js", "fonts", "image", "replaceName")
+);
